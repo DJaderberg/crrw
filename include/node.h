1bd7d627dc19abb237745d03efd2612295746b0e
David Jäderberg <david@jaderberg.net>
2014-10-30 20:49:40 +0100

    Introduce more member elements in Nodes
    They correspond to the parameters defined in the article by Ma et al.
    A class PositionedNode was also introduced, which has pre-defined
    semantics for defining distances between the nodes.

--- include/node.h
+++ include/node.h
@@ -3,9 +3,12 @@
  */
 #include <list>
 #include <memory>
+#include <array>
 
 /**
  * A node containing some number of elements
+ *
+ * @author David Jäderberg
  */
 class Node {
 	public:
@@ -26,9 +29,32 @@ class Node {
 		 *
 		 * @param neighbor A Node that should be a neighbor to the current Node
 		 */
-		virtual void insertNeighbor(std::shared_ptr<Node> neighbor);
+		virtual void insertNeighbor(std::shared_ptr<Node> neighbor); //TODO: Needs more information to be possible (distance, etc)
+		/**
+		 * Allow elements at node to move, if needed
+		 */
+		void takeStep();
+		virtual ~Node() {
+		}
 	private:
+		/**
+		 * A list containing all neighbors of the Node.
+		 */
 		std::list<std::shared_ptr<Node>> neighbors;
+		///The 'length in space' to all neighbors of the node
+		std::list<double> length;
+		///Number of elements at the node
+		double potential = 0; //TODO: Should this be unsigned int?
+		///Flow rate through the node
+		std::list<double> current;
+		///Conductivity of the node
+		std::list<double> conductivity;
+		///Capacitance of the node
+		double capacitance = 0;
+		///Reinforcement intensity
+		constexpr static double q = 0.0001;
+		///Conductivity decrease rate
+		constexpr static double lambda = 0.001;
 };
 
 /**
@@ -50,6 +76,9 @@ class Source : public Node {
 		 * @param p The production rate of the source
 		 */
 		Source(int p) : productionRate(p) {};
+		/**
+		 * Create a new Source with neighbors n and production rate 0
+		 */
 		Source(std::list<std::shared_ptr<Node>> n) : Node(n) {};
 		Source() {};
 		/**
@@ -59,7 +88,44 @@ class Source : public Node {
 		 */
 		int getProductionRate();
 	private:
+		/**
+		 * The production rate of the Source
+		 *
+		 * Any source with a negative production rate can be considered as 
+		 * a sink.
+		 */
 		int productionRate = 0;
 };
 
 
+/**
+ * A node with a position in space
+ *
+ * @tparam dimension Dimensionality of the Euclidean space that the node exists in
+ */
+template<int dimension>
+class PositionedNode : public Node {
+	public:
+		/**
+		 * Create a PositionedNode
+		 *
+		 * @param pos The position
+		 */
+		PositionedNode(std::array<double, dimension> pos) : position(pos) {};
+		/**
+		 * Create a new neighbor of the node
+		 *
+		 * @param n The PositionedNode to be the new neighbor
+		 */
+		void insertNeighbor(std::shared_ptr<PositionedNode> n);
+	private:
+		///Position of the node
+		std::array<PositionedNode, dimension> position;
+		/**
+		 * Calculate the distance from this node to the other
+		 *
+		 * @param n The other node
+		 * @return The Euclidean distance between the nodes
+		 */
+		double distance(PositionedNode<dimension> n);
+};
--- src/node.cpp
+++ src/node.cpp
@@ -1,3 +1,7 @@
+/** @file node.cpp
+ * Contains definitions of functions in Node and Source classes.
+ */
+#include <cmath>
 #include "node.h"
 
 std::list<std::shared_ptr<Node>> Node::getNeighbors() {
@@ -5,9 +9,27 @@ std::list<std::shared_ptr<Node>> Node::getNeighbors() {
 }
 
 void Node::insertNeighbor(std::shared_ptr<Node> neighbor) {
-	this->neighbors.push_back(neighbor);
+	this->neighbors.push_front(neighbor);
 }
 
 int Source::getProductionRate() {
 	return this->productionRate;
 }
+
+template<int dimension>
+void PositionedNode<dimension>::insertNeighbor(std::shared_ptr<PositionedNode<dimension>> n) {
+	this->neighbors.push_front(n);
+	this->length.push_front(this->distance(n));
+}
+
+template<int dimension>
+double PositionedNode<dimension>::distance(PositionedNode<dimension> n) {
+	double dist = 0;
+	double temp;
+	for (int i = 0; i < dimension; ++i) {
+		temp = (this->position[i] - n->position[i]);
+		dist += temp*temp;
+	}
+	return sqrt(dist);
+}
+
