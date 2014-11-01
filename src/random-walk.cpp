3fc2fe877321e3cdd8259bebcf4f432391e16a02
David Jäderberg <david@jaderberg.net>
2014-11-01 11:08:06 +0100

    Create neighbor relation with distance

--- src/random-walk.cpp
+++ src/random-walk.cpp
@@ -7,8 +7,9 @@ int main() {
 	std::cout << "Hello World!\n";
 	std::shared_ptr<Node> a(new Node());
 	std::list<std::shared_ptr<Node>> bNeighbors = std::list<std::shared_ptr<Node>>();
+	std::list<double> bDistances = {1};
 	bNeighbors.push_back(a);
-	std::shared_ptr<Node> b(new Node(bNeighbors));
+	std::shared_ptr<Node> b(new Node(bNeighbors, bDistances));
 	return 0;
 }
 
