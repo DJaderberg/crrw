01bcdd225f045270b3d3dd91e5dbcd426ae2599e
David Jäderberg <david@jaderberg.net>
2014-11-02 10:17:57 +0100

    Move constants to Element, so that it actually works as an interface.
    The downside of this is that it is possible to create several different
    elements with the same parameters, which is unnecessary.

--- include/antElement.h
+++ include/antElement.h
@@ -16,10 +16,5 @@
 
 class AntElement : public Element {
 	public:
-		///Reinforcement intensity
-		constexpr static double q = 0.0001;
-		///Conductivity decrease rate
-		constexpr static double lambda = 0.001;
-		///Road maintenance cost factor
-		constexpr static double mu  = 1.1;
+		AntElement() : Element(0.0001, 0.001, 1.1) {};
 };
--- include/element.h
+++ include/element.h
@@ -9,10 +9,28 @@
 /**
  * An element that belongs to some node
  *
- * @author Kristoffer Jonsson
+ * @author Kristoffer Jonsson, David Jäderberg
  */
 
 class Element {
 public:
-private:
+	///Reinforcement intensity
+	const double q;
+	///Conductivity decrease rate
+	const double lambda;
+	///Road maintenance cost factor
+	const double mu;
+	/**
+	 * Construct an element with certain parameters
+	 *
+	 * Each Element with the same parameters is exactly the same as any other 
+	 * element with the same parameters and can be used interchangably. This 
+	 * means that only one Element with the same parameters ever needs to be 
+	 * constructed.
+	 *
+	 * @param q Reinforcement intensity
+	 * @param lambda Conductivity decrease rate
+	 * @param mu Road maintenance cost factor
+	 */
+	Element(const double q, const double lambda, const double mu) : q(q), lambda(lambda), mu(mu) {};
 };
