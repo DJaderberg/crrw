122caa9aa031f8e665975b7a617447f54118ab5f
krjo <silverjonsson@gmail.com>
2014-11-03 15:23:49 +0100

    Added nodeSet class

---
+++ include/nodeSet.h
@@ -0,0 +1,13 @@
+/** @file nodeSet.h
+ * Contains declarations of Node and its subclass Source
+ */
+//
+//  Created by Kristoffer Jonsson on 03/11/14.
+//
+
+class NodeSet {
+public:
+    
+private:
+    
+};
---
+++ src/nodeSet.cpp
@@ -0,0 +1,8 @@
+/** @file nodeSet.cpp
+ * Contains definitions of functions in Node and Source classes.
+ */
+//
+//  Created by Kristoffer Jonsson on 03/11/14.
+//
+
+#include <stdio.h>
