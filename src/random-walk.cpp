59ce0500bb198256d81ffced16697a8576121fc9
krjo <silverjonsson@gmail.com>
2014-11-10 12:12:36 +0100

    NodeSetGraphics update

--- src/random-walk.cpp
+++ src/random-walk.cpp
@@ -5,7 +5,7 @@
 
 int main() {
 	std::shared_ptr<AntElement> e(new AntElement());
-	std::string filename = "test/nodes.txt";
+	std::string filename = "test/nodes2D.txt";
 	algorithmCreator create = CurrentWalk::create;
 	PositionedNodeSet set = PositionedNodeSet(filename, create, e);
 	std::cout << set.toString();
@@ -13,10 +13,10 @@ int main() {
     graphics.init();
     
 	int j = 0;
-    for (int i = 0; i < 1000; ++i) {
+    for (int i = 0; i < 5000; ++i) {
 		set.takeStep(0.1);
 		std::vector<unsigned int> numPart = set.numberOfParticles();
-		if (i % 1 == 0) {
+		if (i % 5 == 0) {
 			std::cout << "Iteration: " << i << "\n";
 			std::stringstream imgFilename("img/a", std::ios_base::in|std::ios_base::out);
 			imgFilename << "img/a";
