f3717c284df8ba7278f6cfee2876b938e2016d79
David Jäderberg <david@jaderberg.net>
2014-11-10 10:27:33 +0100

    Start work on implementing random walk and update .gitingore a bit

--- include/randomWalk.h
+++ include/randomWalk.h
@@ -5,9 +5,18 @@
 #include <memory>
 #include "algorithm.h"
 
+class RandomWalkSource;
+class RandomWalkSink;
+
 class RandomWalk : public Algorithm {
 public:
 	void prepareStep(double dt);
 	void takeStep(double dt);
+	void initialize(std::shared_ptr<Node> n, std::shared_ptr<Element> element) {
+		this->element = element;
+		this->node = n;
+	}
 private:
+	std::shared_ptr<Element> element;
+	std::shared_ptr<Node> node;
 };
--- src/randomWalk.cpp
+++ src/randomWalk.cpp
@@ -3,3 +3,21 @@
  */
 #include "randomWalk.h"
 
+void RandomWalk::prepareStep(double dt) {
+}
+
+void RandomWalk::takeStep(double dt) {
+}
+
+std::shared_ptr<Algorithm> RandomWalk::create(std::shared_ptr<Node> n, std::shared_ptr<Element> element) {
+	std::shared_ptr<RandomWalk> a;
+	if (dynamic_cast<PositionedSource<2>*>(&*n)) {
+		a.reset(new RandomWalkSource());
+	} else if (dynamic_cast<PositionedSink<2>*>(&*n)) {
+		a.reset(new RandomWalkSink());
+	} else {
+		a.reset(new RandomWalk());
+	}
+	a->initialize(n, element);
+	return a;
+}
