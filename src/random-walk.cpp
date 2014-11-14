76f07eff27e5e9391d66403dd6e9bb30de4c9f04
krjo <silverjonsson@gmail.com>
2014-11-14 11:57:36 +0100

    New main().

--- src/random-walk.cpp
+++ src/random-walk.cpp
@@ -9,15 +9,16 @@ int main() {
     std::shared_ptr<AntElement> e(new AntElement());
     algorithmCreator create = CurrentWalk::create;
     int nCount = 10;
-    int writeInterval = 1;
+    int writeDataInterval = 1;
+    int writeGraphicsInterval = 1;
     
 #ifdef GRAPHICS
     std::string imageSavePath = "img/a";
-    generateGraphics(filename, dataPath, imageSavePath, e, create, nCount, writeInterval);
+    generateGraphics(filename, dataPath, imageSavePath, e, create, nCount/writeDataInterval, writeGraphicsInterval);
 #else
     double dt = 0.1;
-    generateData(filename, dataPath, e, create, nCount, dt, writeInterval);
-    generateData(filename, dataPath, e, create, nCount, dt, writeInterval, dataPath);
+    generateData(filename, dataPath, e, create, nCount, dt, writeDataInterval);
+    generateData(filename, dataPath, e, create, nCount, dt, writeDataInterval, dataPath);
 #endif
     return 0;
 }
