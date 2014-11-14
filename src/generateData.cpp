26c422ceed39ee54408a1474ec314429897560f7
krjo <silverjonsson@gmail.com>
2014-11-14 11:36:48 +0100

    New data generating function for starting from already existing data files.

--- src/generateData.cpp
+++ src/generateData.cpp
@@ -32,8 +32,43 @@ void generateData(std::string nodePath, std::string dataSavePath, std::shared_pt
 }
     ofs.close();
     
+    std::string dataSavePathLast = dataSavePath;
+    int pos = dataSavePathLast.find_last_of('.');
+    dataSavePathLast.insert(pos,"LAST");
+    
+    std::ofstream ofsLast(dataSavePathLast);
+    set.writeData(ofsLast);
+    ofsLast.close();
 }
 
+
+/**
+ * Function for generating data, starting from dataReadPath
+ */
+void generateData(std::string nodePath, std::string dataSavePath, std::shared_ptr<AntElement> e, algorithmCreator create, int nCount, double dt, int writeInterval, std::string dataReadPath) {
+    PositionedNodeSet set = PositionedNodeSet(nodePath, create, e);
+    
+    std::ifstream ifs(dataReadPath);
+    set.readData(ifs);
+    ifs.close();
+    std::ofstream ofs(dataSavePath);
+    
+#pragma omp parallel
+    {
+        for (int i = 0; i < nCount; ++i) {
+#pragma omp single
+            {
+                set.takeStep(dt);
+                if (i % writeInterval == 0) {
+                    set.writeData(ofs);
+                    ofs << "\n";
+                }
+            }
+        }
+    }
+    ofs.close();
+    
+}
 
 
 
--- src/random-walk.cpp
+++ src/random-walk.cpp
@@ -8,8 +8,8 @@ int main() {
     std::string dataPath = "data/save.txt";
     std::shared_ptr<AntElement> e(new AntElement());
     algorithmCreator create = CurrentWalk::create;
-    int nCount = 1000;
-    int writeInterval = 5;
+    int nCount = 10;
+    int writeInterval = 1;
     
 #ifdef GRAPHICS
     std::string imageSavePath = "img/a";
@@ -17,6 +17,7 @@ int main() {
 #else
     double dt = 0.1;
     generateData(filename, dataPath, e, create, nCount, dt, writeInterval);
+    generateData(filename, dataPath, e, create, nCount, dt, writeInterval, dataPath);
 #endif
     return 0;
 }
