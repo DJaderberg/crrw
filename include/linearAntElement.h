573bf3b1b7a9e8a86034f8a282ab5ad082dbe28e
krjo <silverjonsson@gmail.com>
2014-11-18 15:35:28 +0100

    LinearAntElement

---
+++ include/linearAntElement.h
@@ -0,0 +1,21 @@
+/** @file linearAntElement.h
+ * Contains declarations of LinearAntElement
+ */
+#pragma once
+#include <list>
+#include <memory>
+#include <array>
+#include "element.h"
+
+/**
+ * An LinearAntElement that belongs to some node
+ *
+ * @author Kristoffer Jonsson
+ */
+
+
+class LinearAntElement : public Element {
+public:
+    LinearAntElement() : Element(0.0001, 0.001, 1, 1e-1, 10) {};
+};
+
---
+++ src/linearAntElement.cpp
@@ -0,0 +1,5 @@
+/** @file linearAntElement.h
+ * Contains definitions of functions in LinearAntElement
+ */
+#include "linearAntElement.h"
+
