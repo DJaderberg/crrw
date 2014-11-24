e7bf7a44d522ca1367804368304df32c44fc4778
David Jäderberg <david@jaderberg.net>
2014-11-24 11:09:59 +0100

    Matrix generating version of the rust script

--- data/rust-tgf/src/main.rs
+++ data/rust-tgf/src/main.rs
@@ -1,4 +1,5 @@
 extern crate getopts;
+extern crate core;
 use getopts::{optopt,optflag,getopts,OptGroup};
 use std::os;
 use std::io::fs::File;
@@ -6,30 +7,34 @@ use std::rand;
 
 fn gen_matrix(size: f64, prod_rate: int, nodes: uint, sinks: uint, mut out: Box<Writer>) {
     //Define Nodes (id, x-pos, y-pos, opt. production rate)
-    let mut i_range = std::iter::range(0u, nodes);
+    let sqrt_nodes : f64 = core::num::Float::sqrt(nodes as f64);
+    let mut i_range = std::iter::range(0u, sqrt_nodes as uint);
     match out.write((nodes.to_string() + "\n").as_bytes()) {
         Ok(()) => (),
         Err(e) => panic!(e.desc)
     };
     for i in i_range {
-        let x_val = size*rand::random::<f64>();
-        let y_val = size*rand::random::<f64>();
-        let mut output : String = i.to_string() + " " + x_val.to_string() + " " + y_val.to_string();
-        if i<= sinks {
-            output = output + " ";
+        let mut j_range = std::iter::range(0u, sqrt_nodes as uint);
+        for j in j_range {
+            let x_val = (i as f64)*size/sqrt_nodes + 0.1*size/sqrt_nodes*(rand::random::<f64>() - 0.5) + (if i%2 == 0 { 0.0 } else { 0.0 });
+            let y_val = (j as f64)*size/sqrt_nodes + 0.1*size/sqrt_nodes*(rand::random::<f64>() - 0.5) + (if i%2 == 0 { 0.0 } else { size/sqrt_nodes*0.5 });
+            let mut output : String = (i*(sqrt_nodes as uint)+j).to_string() + " " + x_val.to_string() + " " + y_val.to_string();
+            if i<= sinks {
+                output = output + " ";
+            }
+            //If Source
+            if i+j==0 {
+                output = output + prod_rate.to_string();
+                //If Sink
+            } else if i==0 && i+j <= sinks {
+                output = output + "-" + (prod_rate/sinks as int).to_string();
+            }
+            output = output + "\n";
+            match out.write(output.as_bytes()) {
+                Ok(()) => (),
+                Err(e) => panic!(e.desc)
+            };
         }
-        //If Source
-        if i==0 {
-            output = output + prod_rate.to_string();
-        //If Sink
-        } else if i <= sinks {
-            output = output + "-" + (prod_rate/sinks as int).to_string();
-        }
-        output = output + "\n";
-        match out.write(output.as_bytes()) {
-            Ok(()) => (),
-            Err(e) => panic!(e.desc)
-        };
     }
     return;
 }
@@ -56,13 +61,13 @@ fn main() {
         optopt("d", "sinks", "number of sinks in the box", "SINKS"),
         optflag("h", "help", "print this help menu")
     ];
-    let matches = match getopts(args.tail(), opts) {
+    let matches = match getopts(args.tail(), &opts) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
     };
     //Print help, if needed
     if matches.opt_present("h") {
-        print_usage(program.as_slice(), opts);
+        print_usage(program.as_slice(), &opts);
         return;
     }
     let prod_rate = match matches.opt_str("p") {
